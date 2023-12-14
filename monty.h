51c1fbbd9c8f4e2fa66c6efa46d59af142b1ecf4
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2023-12-14 13:33:32 -0800

    function

--- monty.h
+++ monty.h
@@ -72,7 +72,7 @@ void free_stack(stack_t *head);
 
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **head, unsigned int counter);
+void _pint(stack_t **head, unsigned int line_number);
 
 /* HELPER FUNCTIONS */
 
